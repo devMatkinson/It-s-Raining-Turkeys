acf2d58a6fd4994b5294ba83e4957c30ae59f106
devMatkinson <matthew.atkinson@citypolyhs.org>
2023-11-09 16:56:39 -0500

    make player

---
+++ player.js
@@ -0,0 +1,9 @@
+const player= document.getElementById("player")
+document.addEventListener("keydown", (event)=>{
+    if(event.keycode = 37){
+        player.style.left = -200
+        console.log("move left")
+    }
+    console.log("move left")
+    
+})
