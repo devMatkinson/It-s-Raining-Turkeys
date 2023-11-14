c653feda80f31ad8626fd82a6d4e22375243306a
devMatkinson <matthew.atkinson@citypolyhs.org>
2023-11-14 17:00:19 -0500

    Turkey

---
+++ Turkeys.js
@@ -0,0 +1,9 @@
+const turkeyContainer = documentgetByid("turkey")
+
+setTimeout(()=>{
+    const turkey = document.createElement("div")
+    turkeyContainer.addElement(turkey)
+
+    
+}) ;(5000/60)
+ 
