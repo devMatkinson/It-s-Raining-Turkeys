5424864df24ffa598dd1d5a7a9d604707c82c46a
iiiAst <nathan_stephen@icloud.com>
2023-11-14 16:53:04 -0500

    turkeydies

--- turkeysound.js
+++ turkeysound.js
@@ -1,7 +1,12 @@
-// setInterval(() => {
-//     new Audio('It-s-Raining-Turkeys/Turkey Gobble-SoundBible.com-123256561.mp3').play()
+ var turkeybutton = document.getElementById("playbutton")
+turkeybutton.addEventListener("click",()=>{
+    console.log('thisworks')
+    setInterval(() => {
+       new Audio('turkeycaught.mp3').play()
+    
+    }, 2000);
+})
 
-// }, 2000);
 document.addEventListener('keydown',(key)=>{
     if(key.keyCode ===37){
         new Audio('turkeysound.mp3').play()
