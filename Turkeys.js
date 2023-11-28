5763ff46ef51cd617ad3a82797880bbcf49ca931
devMatkinson <matthew.atkinson@citypolyhs.org>
2023-11-28 17:02:06 -0500

    some edits to turkey

--- Turkeys.js
+++ Turkeys.js
@@ -1,9 +1,11 @@
 const turkeyContainer = documentgetByid("turkey")
+const turkeyContainer = document.getElementById("turkeyContainer");
+const player = document,getElementById("turkeyContainer");
 
 setTimeout(()=>{
     const turkey = document.createElement("div")
     turkeyContainer.addElement(turkey)
 
-    
+//Five seconds untill the turkey reaches the bottom
 }) ;(5000/60)
  
