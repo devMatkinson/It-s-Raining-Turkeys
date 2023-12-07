e9ec1d5e57ece7fe327c8b8d6416ec517e8c6b42
devMatkinson <matthew.atkinson@citypolyhs.org>
2023-12-07 15:54:54 -0500

    the turkey is falling!

--- Turkeys.js
+++ Turkeys.js
@@ -1,13 +1,14 @@
 const turkeyContainer = document.getElementById("turkeyContainer");
 const player = document.getElementById("turkeyContainer");
-
+let speed = 10
+const maxSpeed = 50
 setTimeout(()=>{
     const turkey = createTurkey()
     turkeyContainer.appendChild(turkey)
-    ;(2000/60)
+    ;(100/60)
 //Five seconds untill the turkey reaches the bottom of the gameboard
 }) 
-
+//Code for the turkey
 function createTurkey(){
     const turkey = document.createElement("div")
     turkey.style.width="30px"
@@ -18,10 +19,18 @@ function createTurkey(){
     turkey.style.left=Math.random() *100 + "px"
     var yPosition=20
     setInterval(()=>{
-        yPosition+=10
+        if(yPosition>=670){
+            if(speed<=maxSpeed){
+                speed+=2
+            }
+            
+            yPosition=0
+            turkey.style.transform =`translateY(${yPosition+"px"})`
+        }
+        yPosition+=speed
         turkey.style.transform =`translateY(${yPosition+"px"})`
         console.log(yPosition)
-    },300)
+    },100)
     
     return turkey
 }
